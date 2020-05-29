8a563337adab43d91a944be3c45df87c0ca54e7a
Brandon Baker <niea@howold.ru>
2020-05-29 04:50:19 -0400

    asdsdfg

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nieaHax</title>
+    <link rel="stylesheet" href="https://raw.githubusercontent.com/necolas/normalize.css/master/normalize.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <taskbar>
+        <tag>niea<span>Hax</span></tag>
+    </taskbar>
+    <content>
+        <window>
+            <titlebar>
+                <name></name>
+            </titlebar>
+        </window>
+    </content>
+</body>
+</html>
