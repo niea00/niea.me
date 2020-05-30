ebde4ee7ddeab6a3effb7f86461fd4e46573538a
Brandon Baker <niea@howold.ru>
2020-05-29 22:49:16 -0400

    complete site overhaul, added desktop style navigation, window system, and stream notifications. still have to add functionality to site

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+
+$live = false;
+
+$API_KEY = 'AIzaSyA7ddlax4__lfpWFITD_k1doQgyQNult5A'; 
+$ChannelID = 'UCSJ4gkVC6NrvII8umztf0Ow'; 
+$channelInfo = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId='.$ChannelID.'&type=video&eventType=live&key='.$API_KEY; 
+$extractInfo = file_get_contents($channelInfo); 
+$extractInfo = str_replace('},]',"}]",$extractInfo); 
+$showInfo = json_decode($extractInfo, true); 
+$live = $showInfo['pageInfo']['totalResults'] !== 0;
+
+if($live){
+$livetitle = $showInfo['items'][0]['snippet']['title'];
+$liveurl = "https://youtube.com/watch?v=".$showInfo['items'][0]['id']['videoId'];
+$livethumb = $showInfo['items'][0]['snippet']['thumbnails']['medium']['url'];
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,18 +25,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>nieaHax</title>
     <link rel="stylesheet" href="https://raw.githubusercontent.com/necolas/normalize.css/master/normalize.css">
+    <link rel="stylesheet" type="text/css" href="jqui.css"/>
     <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 </head>
 <body>
     <taskbar>
+        <a href="#">
         <tag>niea<span>Hax</span></tag>
+        </a>
+            <live <?php if($live){echo 'class="islive"';}?>>LIVE</live>
+            <popup <?php if(!$live){echo 'style="display: none;"';}?>>
+                <close></close>
+                niea is currently live on YouTube!
+                <a href="<?php echo $liveurl; ?>">
+                <img src="<?php echo $livethumb; ?>">
+                <livetitle><?php echo $livetitle;?></livetitle>
+                </a>
+            </popup>
+        <clock>00:00:00</clock>
     </taskbar>
     <content>
-        <window>
+        <dwm>
+            <icon id="steam">
+                <img src="https://cdn.freebiesupply.com/images/large/2x/steam-logo-transparent.png">
+                <name>Steam</name>
+            </icon>
+            <icon id="file">
+                <img src="https://apprecs.org/gp/images/app-icons/300/c4/com.lenovo.FileBrowser2.jpg">
+                <name>File</name>
+            </icon>
+            <icon id="csgo">
+                <img src="https://lookingforclan.com/sites/default/files/styles/icon/public/2018-02/csgo-icon.jpg?itok=QowWchnK">
+                <name>CSGO</name>
+            </icon>
+        </dwm>
+        <window id="steam" data-minw="200" data-minh="120">
             <titlebar>
-                <name></name>
+                <name>Steam</name>
+                <closebutton></closebutton>
             </titlebar>
+            <wincontent>
+                content here :))
+            </wincontent>
+        </window>
+        <window id="file" data-minw="200" data-minh="120">
+            <titlebar>
+                <name>File</name>
+                <closebutton></closebutton>
+            </titlebar>
+            <wincontent>
+                content file  here :))
+            </wincontent>
+        </window>
+        <window id="csgo" data-minw="200" data-minh="120">
+            <titlebar>
+                <name>CSGO</name>
+                <closebutton></closebutton>
+            </titlebar>
+            <wincontent>
+                content file  here :))
+            </wincontent>
         </window>
     </content>
+    <script type="text/javascript" src="script.js"></script>
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,99 @@
+let globalZ = 10;
+
+$('icon').dblclick((e) => {
+    openWindow(e.currentTarget.id);
+});
+
+$('window').draggable({
+    containment: "content",
+    handle: "titlebar",
+    stop: function(e, ui){
+        updateWindowPos(ui.helper[0].id);
+    }
+}).resizable({
+    containment: "content",
+    stop: function(e, ui){
+        updateWindowPos(ui.helper[0].id);
+    }
+});
+
+$('window').mousedown((e) => {
+    $('window').removeClass("focus");
+    $('window#' + e.currentTarget.id)
+    .css({'z-index': globalZ++})
+    .addClass("focus")
+});
+
+$('close').click(() => {
+    $('popup').animate({
+        right: "-50%"
+    }, 500, () => {
+        $('popup').hide().css('right', '4px');
+    })
+})
+
+$('live').click(() => {
+    $('popup').fadeIn(100);
+})
+
+$('closebutton').click((e) => {
+    $('window#' + e.currentTarget.parentNode.parentNode.id).hide();
+});
+
+$('dwm').mousedown(() => {
+    $('window').removeClass("focus");
+});
+
+$('titlebar name').css(
+    'background-image',
+    function() {
+        return 'url(' + $('icon#' + $(this).parent().parent().attr('id') + ' img').attr('src') + ')'
+    } 
+)
+
+console.log($('titlebar name'))
+
+startTime();
+
+ws = window.localStorage;
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    $('clock').text(h + ":" + m + ":" + s);
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+function openWindow(id){
+    if(ws.getItem(id + '.x')){
+        $('window#' + id)       
+        .css({'width': ws.getItem(id + ".w"), 'height': ws.getItem(id + ".h")})
+        .css({'top': ws.getItem(id + ".y"), 'left': ws.getItem(id + ".x")})
+        .css({'display': 'initial'})
+        .addClass("focus")
+        .css({'z-index': globalZ++})
+    } else {
+        $('window#' + id)
+        .css({'width': $('window#' + id).data('minw') + "px", 'height': $('window#' + id).data('minh') + "px"})
+        .css({'top': ($('dwm').height()/2)-($('window#' + id).data('minh')/2), 'left': ($('dwm').width()/2)-($('window#' + id).data('minw')/2)})
+        .css({'display': 'initial'})
+        .addClass("focus")
+        .css({'z-index': globalZ++})
+    }
+}
+
+function updateWindowPos(id){
+    ws.setItem(id+'.x', $("window#" + id).css('left'));
+    ws.setItem(id+'.y', $("window#" + id).css('top'));
+    ws.setItem(id+'.w', $("window#" + id).css('width'));
+    ws.setItem(id+'.h', $("window#" + id).css('height'));
+}
